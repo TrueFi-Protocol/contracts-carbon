cf6e7da5360acae5680dc9fafbcc605013cc4211
truefibot <truebot.github@trusttoken.com>
2022-11-23 13:37:41 +0000

    6️⃣ Test portfolio with different token decimals (#4658)

--- test/fixtures/structuredPortfolioFactoryFixture.ts
+++ test/fixtures/structuredPortfolioFactoryFixture.ts
@@ -43,212 +43,215 @@ interface PortfolioParams {
   minimumSize: number,
 }
 
-export const structuredPortfolioFactoryFixture = async ([wallet, other]: Wallet[]) => {
-  const tokenDecimals = 6
-  const token = await new MockToken__factory(wallet).deploy(tokenDecimals)
+export const getStructuredPortfolioFactoryFixture = (tokenDecimals: number) => {
+  return async ([wallet, other]: Wallet[]) => {
+    const token = await new MockToken__factory(wallet).deploy(tokenDecimals)
 
-  const parseTokenUnits = (amount: string | number) => utils.parseUnits(amount.toString(), tokenDecimals)
+    const parseTokenUnits = (amount: string | number) => utils.parseUnits(amount.toString(), tokenDecimals)
 
-  await token.mint(wallet.address, parseTokenUnits(1e12))
-  await token.mint(other.address, parseTokenUnits(1e10))
+    await token.mint(wallet.address, parseTokenUnits(1e12))
+    await token.mint(other.address, parseTokenUnits(1e10))
 
-  const structuredPortfolioImplementation = await new StructuredPortfolioTest__factory(wallet).deploy()
-  const trancheVaultImplementation = await new TrancheVaultTest__factory(wallet).deploy()
+    const structuredPortfolioImplementation = await new StructuredPortfolioTest__factory(wallet).deploy()
+    const trancheVaultImplementation = await new TrancheVaultTest__factory(wallet).deploy()
 
-  const { protocolConfig, protocolConfigParams } = await deployProtocolConfig(wallet)
+    const { protocolConfig, protocolConfigParams } = await deployProtocolConfig(wallet)
 
-  const structuredPortfolioFactory = await new StructuredPortfolioFactory__factory(wallet).deploy(
-    structuredPortfolioImplementation.address,
-    trancheVaultImplementation.address,
-    protocolConfig.address,
-  )
-
-  const whitelistedManagerRole = await structuredPortfolioFactory.WHITELISTED_MANAGER_ROLE()
-  await structuredPortfolioFactory.grantRole(whitelistedManagerRole, wallet.address)
-
-  const { fixedInterestOnlyLoans } = await deployFixedInterestOnlyLoans([wallet])
-
-  const { depositController, withdrawController, transferController } = await deployControllers(wallet)
-
-  const lenderVerifier = await new AllowAllLenderVerifier__factory(wallet).deploy()
-
-  const sizes = [
-    { floor: constants.Zero, ceiling: parseTokenUnits(5e9) },
-    { floor: constants.Zero, ceiling: parseTokenUnits(5e9) },
-    { floor: constants.Zero, ceiling: parseTokenUnits(1e10) },
-  ]
-
-  const equityTrancheInitData: TrancheInitData = {
-    name: 'Equity Tranche',
-    symbol: 'EQT',
-    depositControllerImplementation: depositController.address,
-    depositControllerInitData: depositController.interface.encodeFunctionData('initialize', [wallet.address, lenderVerifier.address, 0, parseTokenUnits(1e10)]),
-    withdrawControllerImplementation: withdrawController.address,
-    withdrawControllerInitData: withdrawController.interface.encodeFunctionData('initialize', [wallet.address, 0, 1]),
-    transferControllerImplementation: transferController.address,
-    transferControllerInitData: transferController.interface.encodeFunctionData('initialize', [wallet.address]),
-    targetApy: 0,
-    minSubordinateRatio: 0,
-    managerFeeRate: 0,
-  }
-
-  const juniorTrancheInitData: TrancheInitData = {
-    name: 'Junior Tranche',
-    symbol: 'JNT',
-    depositControllerImplementation: depositController.address,
-    depositControllerInitData: depositController.interface.encodeFunctionData('initialize', [wallet.address, lenderVerifier.address, 0, parseTokenUnits(1e10)]),
-    withdrawControllerImplementation: withdrawController.address,
-    withdrawControllerInitData: withdrawController.interface.encodeFunctionData('initialize', [wallet.address, 0, 1]),
-    transferControllerImplementation: transferController.address,
-    transferControllerInitData: transferController.interface.encodeFunctionData('initialize', [wallet.address]),
-    targetApy: 500,
-    minSubordinateRatio: 0,
-    managerFeeRate: 0,
-  }
-
-  const seniorTrancheInitData: TrancheInitData = {
-    name: 'Senior Tranche',
-    symbol: 'SNT',
-    depositControllerImplementation: depositController.address,
-    depositControllerInitData: depositController.interface.encodeFunctionData('initialize', [wallet.address, lenderVerifier.address, 0, parseTokenUnits(1e10)]),
-    withdrawControllerImplementation: withdrawController.address,
-    withdrawControllerInitData: withdrawController.interface.encodeFunctionData('initialize', [wallet.address, 0, 1]),
-    transferControllerImplementation: transferController.address,
-    transferControllerInitData: transferController.interface.encodeFunctionData('initialize', [wallet.address]),
-    targetApy: 300,
-    minSubordinateRatio: 0,
-    managerFeeRate: 0,
-  }
-
-  const tranchesInitData = [
-    equityTrancheInitData,
-    juniorTrancheInitData,
-    seniorTrancheInitData,
-  ]
-
-  const portfolioDuration = 2 * YEAR
-
-  const portfolioParams: PortfolioParams = {
-    name: 'Portfolio',
-    duration: portfolioDuration,
-    capitalFormationPeriod: 90 * DAY,
-    minimumSize: 0,
-  }
-
-  const expectedEquityRate = { from: 200, to: 2000 }
-
-  async function createPortfolio(params: Partial<{
-    token: Wallet | Contract,
-    fixedInterestOnlyLoans: Wallet | Contract,
-    portfolioParams: PortfolioParams,
-    tranchesInitData: TrancheInitData[],
-    expectedEquityRate: { from: number, to: number },
-  }> = {}) {
-    const args = {
-      token,
-      fixedInterestOnlyLoans,
-      portfolioParams,
-      tranchesInitData,
-      expectedEquityRate,
-      ...params,
-    }
-    const createPortfolioTx = await structuredPortfolioFactory.createPortfolio(
-      args.token.address,
-      args.fixedInterestOnlyLoans.address,
-      args.portfolioParams,
-      args.tranchesInitData,
-      args.expectedEquityRate,
+    const structuredPortfolioFactory = await new StructuredPortfolioFactory__factory(wallet).deploy(
+      structuredPortfolioImplementation.address,
+      trancheVaultImplementation.address,
+      protocolConfig.address,
     )
-    const portfolio = await getPortfolioFromTx(createPortfolioTx)
 
-    return { portfolio, createPortfolioTx }
-  }
+    const whitelistedManagerRole = await structuredPortfolioFactory.WHITELISTED_MANAGER_ROLE()
+    await structuredPortfolioFactory.grantRole(whitelistedManagerRole, wallet.address)
 
-  async function createPortfolioAndSetupControllers(...args: Parameters<typeof createPortfolio>) {
-    const { portfolio, createPortfolioTx } = await createPortfolio(...args)
+    const { fixedInterestOnlyLoans } = await deployFixedInterestOnlyLoans([wallet])
+
+    const { depositController, withdrawController, transferController } = await deployControllers(wallet)
+
+    const lenderVerifier = await new AllowAllLenderVerifier__factory(wallet).deploy()
+
+    const sizes = [
+      { floor: constants.Zero, ceiling: parseTokenUnits(5e9) },
+      { floor: constants.Zero, ceiling: parseTokenUnits(5e9) },
+      { floor: constants.Zero, ceiling: parseTokenUnits(1e10) },
+    ]
+
+    const equityTrancheInitData: TrancheInitData = {
+      name: 'Equity Tranche',
+      symbol: 'EQT',
+      depositControllerImplementation: depositController.address,
+      depositControllerInitData: depositController.interface.encodeFunctionData('initialize', [wallet.address, lenderVerifier.address, 0, parseTokenUnits(1e10)]),
+      withdrawControllerImplementation: withdrawController.address,
+      withdrawControllerInitData: withdrawController.interface.encodeFunctionData('initialize', [wallet.address, 0, 1]),
+      transferControllerImplementation: transferController.address,
+      transferControllerInitData: transferController.interface.encodeFunctionData('initialize', [wallet.address]),
+      targetApy: 0,
+      minSubordinateRatio: 0,
+      managerFeeRate: 0,
+    }
+
+    const juniorTrancheInitData: TrancheInitData = {
+      name: 'Junior Tranche',
+      symbol: 'JNT',
+      depositControllerImplementation: depositController.address,
+      depositControllerInitData: depositController.interface.encodeFunctionData('initialize', [wallet.address, lenderVerifier.address, 0, parseTokenUnits(1e10)]),
+      withdrawControllerImplementation: withdrawController.address,
+      withdrawControllerInitData: withdrawController.interface.encodeFunctionData('initialize', [wallet.address, 0, 1]),
+      transferControllerImplementation: transferController.address,
+      transferControllerInitData: transferController.interface.encodeFunctionData('initialize', [wallet.address]),
+      targetApy: 500,
+      minSubordinateRatio: 0,
+      managerFeeRate: 0,
+    }
+
+    const seniorTrancheInitData: TrancheInitData = {
+      name: 'Senior Tranche',
+      symbol: 'SNT',
+      depositControllerImplementation: depositController.address,
+      depositControllerInitData: depositController.interface.encodeFunctionData('initialize', [wallet.address, lenderVerifier.address, 0, parseTokenUnits(1e10)]),
+      withdrawControllerImplementation: withdrawController.address,
+      withdrawControllerInitData: withdrawController.interface.encodeFunctionData('initialize', [wallet.address, 0, 1]),
+      transferControllerImplementation: transferController.address,
+      transferControllerInitData: transferController.interface.encodeFunctionData('initialize', [wallet.address]),
+      targetApy: 300,
+      minSubordinateRatio: 0,
+      managerFeeRate: 0,
+    }
+
+    const tranchesInitData = [
+      equityTrancheInitData,
+      juniorTrancheInitData,
+      seniorTrancheInitData,
+    ]
+
+    const portfolioDuration = 2 * YEAR
+
+    const portfolioParams: PortfolioParams = {
+      name: 'Portfolio',
+      duration: portfolioDuration,
+      capitalFormationPeriod: 90 * DAY,
+      minimumSize: 0,
+    }
+
+    const expectedEquityRate = { from: 200, to: 2000 }
+
+    async function createPortfolio(params: Partial<{
+      token: Wallet | Contract,
+      fixedInterestOnlyLoans: Wallet | Contract,
+      portfolioParams: PortfolioParams,
+      tranchesInitData: TrancheInitData[],
+      expectedEquityRate: { from: number, to: number },
+    }> = {}) {
+      const args = {
+        token,
+        fixedInterestOnlyLoans,
+        portfolioParams,
+        tranchesInitData,
+        expectedEquityRate,
+        ...params,
+      }
+      const createPortfolioTx = await structuredPortfolioFactory.createPortfolio(
+        args.token.address,
+        args.fixedInterestOnlyLoans.address,
+        args.portfolioParams,
+        args.tranchesInitData,
+        args.expectedEquityRate,
+      )
+      const portfolio = await getPortfolioFromTx(createPortfolioTx)
+
+      return { portfolio, createPortfolioTx }
+    }
+
+    async function createPortfolioAndSetupControllers(...args: Parameters<typeof createPortfolio>) {
+      const { portfolio, createPortfolioTx } = await createPortfolio(...args)
+      const tranches = await getTranchesFromTx(createPortfolioTx)
+      const controllers: {depositController: DepositController, withdrawController: WithdrawController}[] = []
+      for (let i = 0; i < tranches.length; i++) {
+        const depositControllerAddress = await tranches[i].depositController()
+        const withdrawControllerAddress = await tranches[i].withdrawController()
+        const depositController = DepositController__factory.connect(depositControllerAddress, wallet)
+        const withdrawController = WithdrawController__factory.connect(withdrawControllerAddress, wallet)
+        controllers.push({ depositController, withdrawController })
+      }
+      return { portfolio, tranches, createPortfolioTx, controllers }
+    }
+
+    const { createPortfolioTx } = await createPortfolio()
+
+    const { timestamp: now } = await wallet.provider.getBlock('latest')
+    const maxCapitalFormationDuration = 90 * DAY
+    const startDeadline = now + maxCapitalFormationDuration
+
     const tranches = await getTranchesFromTx(createPortfolioTx)
-    const controllers: {depositController: DepositController, withdrawController: WithdrawController}[] = []
+
+    const tranchesData: TrancheData[] = []
     for (let i = 0; i < tranches.length; i++) {
       const depositControllerAddress = await tranches[i].depositController()
       const withdrawControllerAddress = await tranches[i].withdrawController()
       const depositController = DepositController__factory.connect(depositControllerAddress, wallet)
       const withdrawController = WithdrawController__factory.connect(withdrawControllerAddress, wallet)
-      controllers.push({ depositController, withdrawController })
+      tranchesData.push({
+        ...tranchesInitData[i],
+        depositController,
+        withdrawController,
+      })
+
+      await depositController.setCeiling(sizes[i].ceiling)
+      await withdrawController.setFloor(sizes[i].floor)
     }
-    return { portfolio, tranches, createPortfolioTx, controllers }
-  }
 
-  const { createPortfolioTx } = await createPortfolio()
+    async function depositToTranche(tranche: TrancheVaultTest, amount: BigNumberish, receiver = wallet.address) {
+      await token.approve(tranche.address, amount)
+      return tranche.deposit(amount, receiver)
+    }
 
-  const { timestamp: now } = await wallet.provider.getBlock('latest')
-  const maxCapitalFormationDuration = 90 * DAY
-  const startDeadline = now + maxCapitalFormationDuration
+    async function mintToTranche(tranche: TrancheVaultTest, shares: BigNumberish, receiver = wallet.address) {
+      await token.approve(tranche.address, constants.MaxUint256)
+      return tranche.mint(shares, receiver)
+    }
 
-  const tranches = await getTranchesFromTx(createPortfolioTx)
+    async function getPortfolioFromTx(tx: ContractTransaction = createPortfolioTx) {
+      const portfolioAddress: string = await extractEventArgFromTx(tx, [structuredPortfolioFactory.address, 'PortfolioCreated', 'newPortfolio'])
+      return new StructuredPortfolioTest__factory(wallet).attach(portfolioAddress)
+    }
 
-  const tranchesData: TrancheData[] = []
-  for (let i = 0; i < tranches.length; i++) {
-    const depositControllerAddress = await tranches[i].depositController()
-    const withdrawControllerAddress = await tranches[i].withdrawController()
-    const depositController = DepositController__factory.connect(depositControllerAddress, wallet)
-    const withdrawController = WithdrawController__factory.connect(withdrawControllerAddress, wallet)
-    tranchesData.push({
-      ...tranchesInitData[i],
-      depositController,
-      withdrawController,
-    })
+    async function getTranchesFromTx(tx: ContractTransaction = createPortfolioTx) {
+      const tranchesAddresses: string[] = await extractEventArgFromTx(tx, [structuredPortfolioFactory.address, 'PortfolioCreated', 'tranches'])
+      const tranches = tranchesAddresses.map((address) => new TrancheVaultTest__factory(wallet).attach(address))
+      return tranches
+    }
 
-    await depositController.setCeiling(sizes[i].ceiling)
-    await withdrawController.setFloor(sizes[i].floor)
-  }
-
-  async function depositToTranche(tranche: TrancheVaultTest, amount: BigNumberish, receiver = wallet.address) {
-    await token.approve(tranche.address, amount)
-    return tranche.deposit(amount, receiver)
-  }
-
-  async function mintToTranche(tranche: TrancheVaultTest, shares: BigNumberish, receiver = wallet.address) {
-    await token.approve(tranche.address, constants.MaxUint256)
-    return tranche.mint(shares, receiver)
-  }
-
-  async function getPortfolioFromTx(tx: ContractTransaction = createPortfolioTx) {
-    const portfolioAddress: string = await extractEventArgFromTx(tx, [structuredPortfolioFactory.address, 'PortfolioCreated', 'newPortfolio'])
-    return new StructuredPortfolioTest__factory(wallet).attach(portfolioAddress)
-  }
-
-  async function getTranchesFromTx(tx: ContractTransaction = createPortfolioTx) {
-    const tranchesAddresses: string[] = await extractEventArgFromTx(tx, [structuredPortfolioFactory.address, 'PortfolioCreated', 'tranches'])
-    const tranches = tranchesAddresses.map((address) => new TrancheVaultTest__factory(wallet).attach(address))
-    return tranches
-  }
-
-  return {
-    structuredPortfolioFactory,
-    tranchesData,
-    tranches,
-    token,
-    fixedInterestOnlyLoans,
-    portfolioDuration,
-    portfolioParams,
-    createPortfolioTx,
-    parseTokenUnits,
-    depositToTranche,
-    mintToTranche,
-    getPortfolioFromTx,
-    getTranchesFromTx,
-    startDeadline,
-    maxCapitalFormationDuration,
-    equityTrancheData: tranchesData[0],
-    juniorTrancheData: tranchesData[1],
-    seniorTrancheData: tranchesData[2],
-    whitelistedManagerRole,
-    protocolConfig,
-    protocolConfigParams,
-    expectedEquityRate,
-    tranchesInitData,
-    createPortfolio,
-    createPortfolioAndSetupControllers,
-    lenderVerifier,
+    return {
+      structuredPortfolioFactory,
+      tranchesData,
+      tranches,
+      token,
+      fixedInterestOnlyLoans,
+      portfolioDuration,
+      portfolioParams,
+      createPortfolioTx,
+      parseTokenUnits,
+      depositToTranche,
+      mintToTranche,
+      getPortfolioFromTx,
+      getTranchesFromTx,
+      startDeadline,
+      maxCapitalFormationDuration,
+      equityTrancheData: tranchesData[0],
+      juniorTrancheData: tranchesData[1],
+      seniorTrancheData: tranchesData[2],
+      whitelistedManagerRole,
+      protocolConfig,
+      protocolConfigParams,
+      expectedEquityRate,
+      tranchesInitData,
+      createPortfolio,
+      createPortfolioAndSetupControllers,
+      lenderVerifier,
+    }
   }
 }
+
+export const structuredPortfolioFactoryFixture = getStructuredPortfolioFactoryFixture(6)
--- test/trancheVault/integration.test.ts
+++ test/trancheVault/integration.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 import { BigNumberish, constants, Wallet } from 'ethers'
-import { structuredPortfolioLiveFixture } from 'fixtures/structuredPortfolioFixture'
+import { getStructuredPortfolioLiveFixture } from 'fixtures/structuredPortfolioFixture'
 import { setupFixtureLoader } from 'test/setup'
 import { YEAR } from 'utils/constants'
 import { extractEventArgFromTx } from 'utils/extractEventArgFromTx'
@@ -9,104 +9,108 @@ import { timeTravel } from 'utils/timeTravel'
 describe('TrancheVault integration tests', () => {
   const loadFixture = setupFixtureLoader()
 
-  it('multiple deposits and withdrawals', async () => {
-    const { juniorTranche, provider, parseTokenUnits, token } = await loadFixture(structuredPortfolioLiveFixture)
-    const [lenderA, lenderB, lenderC] = provider.getWallets().slice(3)
+  const tokenDecimals = [6, 8, 18]
 
-    let lenderAShares = constants.Zero
-    let lenderBShares = constants.Zero
-    let lenderCShares = constants.Zero
+  for (const decimals of tokenDecimals) {
+    it(`multiple deposits and withdrawals (${decimals} decimals)`, async () => {
+      const { juniorTranche, provider, parseTokenUnits, token } = await loadFixture(getStructuredPortfolioLiveFixture(decimals))
+      const [lenderA, lenderB, lenderC] = provider.getWallets().slice(3)
 
-    let lenderABalance = constants.Zero
-    let lenderBBalance = constants.Zero
-    let lenderCBalance = constants.Zero
+      let lenderAShares = constants.Zero
+      let lenderBShares = constants.Zero
+      let lenderCShares = constants.Zero
 
-    async function prepare() {
-      for (const lender of [lenderA, lenderB, lenderC]) {
-        await token.mint(lender.address, parseTokenUnits(1e9))
-        await token.connect(lender).approve(juniorTranche.address, constants.MaxUint256)
+      let lenderABalance = constants.Zero
+      let lenderBBalance = constants.Zero
+      let lenderCBalance = constants.Zero
+
+      async function prepare() {
+        for (const lender of [lenderA, lenderB, lenderC]) {
+          await token.mint(lender.address, parseTokenUnits(1e9))
+          await token.connect(lender).approve(juniorTranche.address, constants.MaxUint256)
+        }
+
+        const updateBalances = async () => {
+          lenderAShares = await juniorTranche.balanceOf(lenderA.address)
+          lenderBShares = await juniorTranche.balanceOf(lenderB.address)
+          lenderCShares = await juniorTranche.balanceOf(lenderC.address)
+          lenderABalance = await token.balanceOf(lenderA.address)
+          lenderBBalance = await token.balanceOf(lenderB.address)
+          lenderCBalance = await token.balanceOf(lenderC.address)
+        }
+
+        function depositToTranche(amount: BigNumberish, lender: Wallet) {
+          return juniorTranche.connect(lender).deposit(amount, lender.address)
+        }
+
+        function redeemFromTranche(amount: BigNumberish, owner: Wallet) {
+          return juniorTranche.connect(owner).redeem(amount, owner.address, owner.address)
+        }
+
+        return { updateBalances, depositToTranche, redeemFromTranche }
       }
 
-      const updateBalances = async () => {
-        lenderAShares = await juniorTranche.balanceOf(lenderA.address)
-        lenderBShares = await juniorTranche.balanceOf(lenderB.address)
-        lenderCShares = await juniorTranche.balanceOf(lenderC.address)
-        lenderABalance = await token.balanceOf(lenderA.address)
-        lenderBBalance = await token.balanceOf(lenderB.address)
-        lenderCBalance = await token.balanceOf(lenderC.address)
-      }
+      const { updateBalances, depositToTranche, redeemFromTranche } = await prepare()
 
-      function depositToTranche(amount: BigNumberish, lender: Wallet) {
-        return juniorTranche.connect(lender).deposit(amount, lender.address)
-      }
+      const delta = parseTokenUnits(1)
 
-      function redeemFromTranche(amount: BigNumberish, owner: Wallet) {
-        return juniorTranche.connect(owner).redeem(amount, owner.address, owner.address)
-      }
+      await depositToTranche(parseTokenUnits(1e6), lenderA)
+      await depositToTranche(parseTokenUnits(1e6), lenderB)
+      await updateBalances()
 
-      return { updateBalances, depositToTranche, redeemFromTranche }
-    }
+      expect(lenderAShares).to.be.closeTo(parseTokenUnits(1_000_000), delta)
+      expect(lenderBShares).to.be.closeTo(parseTokenUnits(1_000_000), delta)
+      expect(lenderCShares).to.be.closeTo(0, delta)
 
-    const { updateBalances, depositToTranche, redeemFromTranche } = await prepare()
+      expect(lenderABalance).to.eq(parseTokenUnits(999_000_000))
+      expect(lenderBBalance).to.eq(parseTokenUnits(999_000_000))
+      expect(lenderCBalance).to.eq(parseTokenUnits(1_000_000_000))
 
-    const delta = parseTokenUnits(1).toNumber()
+      await timeTravel(YEAR / 2)
 
-    await depositToTranche(parseTokenUnits(1e6), lenderA)
-    await depositToTranche(parseTokenUnits(1e6), lenderB)
-    await updateBalances()
+      await depositToTranche(parseTokenUnits(1e6), lenderB)
+      await depositToTranche(parseTokenUnits(1e6), lenderC)
+      await updateBalances()
 
-    expect(lenderAShares).to.be.closeTo(parseTokenUnits(1_000_000), delta)
-    expect(lenderBShares).to.be.closeTo(parseTokenUnits(1_000_000), delta)
-    expect(lenderCShares).to.be.closeTo(0, delta)
+      expect(lenderAShares).to.be.closeTo(parseTokenUnits(1_000_000), delta)
+      expect(lenderBShares).to.be.closeTo(parseTokenUnits(1_975_609), delta)
+      expect(lenderCShares).to.be.closeTo(parseTokenUnits(975_609), delta)
 
-    expect(lenderABalance).to.eq(parseTokenUnits(999_000_000))
-    expect(lenderBBalance).to.eq(parseTokenUnits(999_000_000))
-    expect(lenderCBalance).to.eq(parseTokenUnits(1_000_000_000))
+      expect(lenderABalance).to.eq(parseTokenUnits(999_000_000))
+      expect(lenderBBalance).to.eq(parseTokenUnits(998_000_000))
+      expect(lenderCBalance).to.eq(parseTokenUnits(999_000_000))
 
-    await timeTravel(YEAR / 2)
+      await timeTravel(YEAR / 2)
 
-    await depositToTranche(parseTokenUnits(1e6), lenderB)
-    await depositToTranche(parseTokenUnits(1e6), lenderC)
-    await updateBalances()
+      // TODO update rounding in controllers
+      await redeemFromTranche(lenderAShares.sub(1), lenderA)
+      await redeemFromTranche(parseTokenUnits(1e6), lenderB)
+      await updateBalances()
 
-    expect(lenderAShares).to.be.closeTo(parseTokenUnits(1_000_000), delta)
-    expect(lenderBShares).to.be.closeTo(parseTokenUnits(1_975_609), delta)
-    expect(lenderCShares).to.be.closeTo(parseTokenUnits(975_609), delta)
+      expect(lenderAShares).to.eq(1)
+      expect(lenderBShares).to.be.closeTo(parseTokenUnits(975_609), delta)
+      expect(lenderCShares).to.be.closeTo(parseTokenUnits(975_609), delta)
 
-    expect(lenderABalance).to.eq(parseTokenUnits(999_000_000))
-    expect(lenderBBalance).to.eq(parseTokenUnits(998_000_000))
-    expect(lenderCBalance).to.eq(parseTokenUnits(999_000_000))
+      expect(lenderABalance).to.be.closeTo(parseTokenUnits(1_000_050_625), delta)
+      expect(lenderBBalance).to.be.closeTo(parseTokenUnits(999_050_625), delta)
+      expect(lenderCBalance).to.eq(parseTokenUnits(999_000_000))
 
-    await timeTravel(YEAR / 2)
+      await timeTravel(YEAR / 2)
 
-    // TODO update rounding in controllers
-    await redeemFromTranche(lenderAShares.sub(1), lenderA)
-    await redeemFromTranche(parseTokenUnits(1e6), lenderB)
-    await updateBalances()
+      await depositToTranche(parseTokenUnits(1e6), lenderA)
+      const depositTx = await depositToTranche(parseTokenUnits(1e6), lenderB)
+      const mintedShares = await extractEventArgFromTx(depositTx, [juniorTranche.address, 'Deposit', 'shares'])
+      await redeemFromTranche(mintedShares, lenderB)
+      await redeemFromTranche(lenderCShares.div(2), lenderC)
+      await updateBalances()
 
-    expect(lenderAShares).to.eq(1)
-    expect(lenderBShares).to.be.closeTo(parseTokenUnits(975_609), delta)
-    expect(lenderCShares).to.be.closeTo(parseTokenUnits(975_609), delta)
+      expect(lenderAShares).to.be.closeTo(parseTokenUnits(928_599), delta)
+      expect(lenderBShares).to.be.closeTo(parseTokenUnits(975_609), delta)
+      expect(lenderCShares).to.be.closeTo(parseTokenUnits(487_804), delta)
 
-    expect(lenderABalance).to.be.closeTo(parseTokenUnits(1_000_050_625), delta)
-    expect(lenderBBalance).to.be.closeTo(parseTokenUnits(999_050_625), delta)
-    expect(lenderCBalance).to.eq(parseTokenUnits(999_000_000))
-
-    await timeTravel(YEAR / 2)
-
-    await depositToTranche(parseTokenUnits(1e6), lenderA)
-    const depositTx = await depositToTranche(parseTokenUnits(1e6), lenderB)
-    const mintedShares = await extractEventArgFromTx(depositTx, [juniorTranche.address, 'Deposit', 'shares'])
-    await redeemFromTranche(mintedShares, lenderB)
-    await redeemFromTranche(lenderCShares.div(2), lenderC)
-    await updateBalances()
-
-    expect(lenderAShares).to.be.closeTo(parseTokenUnits(928_599), delta)
-    expect(lenderBShares).to.be.closeTo(parseTokenUnits(975_609), delta)
-    expect(lenderCShares).to.be.closeTo(parseTokenUnits(487_804), delta)
-
-    expect(lenderABalance).to.be.closeTo(parseTokenUnits(999_050_625), delta)
-    expect(lenderBBalance).to.be.closeTo(parseTokenUnits(999_050_625), delta)
-    expect(lenderCBalance).to.be.closeTo(parseTokenUnits(999_525_312), delta)
-  })
+      expect(lenderABalance).to.be.closeTo(parseTokenUnits(999_050_625), delta)
+      expect(lenderBBalance).to.be.closeTo(parseTokenUnits(999_050_625), delta)
+      expect(lenderCBalance).to.be.closeTo(parseTokenUnits(999_525_312), delta)
+    })
+  }
 })
