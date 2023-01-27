d98db4b1e3257d8fbeebded1167f1971bf7c4b90
truefibot <truebot.github@trusttoken.com>
2023-01-27 12:26:06 +0000

    ⛽️ Add common manager actions for single tranche gas calculation (#5402)

--- test/gas.test.ts
+++ test/gas.test.ts
@@ -1,4 +1,5 @@
 import { setupFixtureLoader } from './setup'
+import { BigNumber, constants } from 'ethers'
 import { structuredPortfolioFactoryFixture } from 'fixtures/structuredPortfolioFactoryFixture'
 import { ContractTransaction } from 'ethers'
 import { DepositController__factory, WithdrawController__factory } from 'contracts'
@@ -9,6 +10,7 @@ import {
 } from 'fixtures/structuredPortfolioFixture'
 import { depositControllerFixture } from 'fixtures/depositControllerFixture'
 import { withdrawControllerFixture } from 'fixtures/withdrawControllerFixture'
+import { extractEventArgFromTx } from 'utils/extractEventArgFromTx'
 
 const numberWithCommas = (x: string) => x.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 
@@ -208,5 +210,294 @@ describe('Gas cost', () => {
     await depositToTranche(seniorTranche, 100)
     await startAndClosePortfolio()
     await printGasCost(await withdrawFromTranche(seniorTranche, 100))
+  })
+
+  describe('Single tranche', () => {
+    it('create Structured portfolio', async () => {
+      const {
+        structuredPortfolioFactory,
+        token,
+        fixedInterestOnlyLoans,
+        portfolioParams,
+        tranchesInitData,
+        expectedEquityRate,
+      } = await loadFixture(structuredPortfolioLiveFixture)
+
+      const createPortfolioTx = await structuredPortfolioFactory.createPortfolio(
+        token.address,
+        fixedInterestOnlyLoans.address,
+        portfolioParams,
+        [tranchesInitData[0]],
+        expectedEquityRate,
+      )
+
+      await printGasCost(createPortfolioTx)
+    })
+
+    it('start portfolio', async () => {
+      const {
+        structuredPortfolioFactory,
+        token,
+        fixedInterestOnlyLoans,
+        portfolioParams,
+        tranchesInitData,
+        expectedEquityRate,
+        getPortfolioFromTx,
+      } = await loadFixture(structuredPortfolioLiveFixture)
+
+      const createPortfolioTx = await structuredPortfolioFactory.createPortfolio(
+        token.address,
+        fixedInterestOnlyLoans.address,
+        portfolioParams,
+        [tranchesInitData[0]],
+        expectedEquityRate,
+      )
+      const portfolio = await getPortfolioFromTx(createPortfolioTx)
+
+      await printGasCost(await portfolio.start())
+    })
+
+    it('create and disburse loan', async () => {
+      const {
+        structuredPortfolioFactory,
+        token,
+        fixedInterestOnlyLoans,
+        portfolioParams,
+        tranchesInitData,
+        expectedEquityRate,
+        wallet,
+        other,
+        loan,
+        getTranchesFromTx,
+        getPortfolioFromTx,
+        depositToTranche,
+      } = await loadFixture(structuredPortfolioLiveFixture)
+
+      const createPortfolioTx = await structuredPortfolioFactory.createPortfolio(
+        token.address,
+        fixedInterestOnlyLoans.address,
+        portfolioParams,
+        [tranchesInitData[0]],
+        expectedEquityRate,
+      )
+      const portfolio = await getPortfolioFromTx(createPortfolioTx)
+      const [tranche] = await getTranchesFromTx(createPortfolioTx)
+      const depositControllerAddress = await tranche.depositController()
+      const depositController = DepositController__factory.connect(depositControllerAddress, wallet)
+      await depositController.setCeiling(loan.principal)
+      await portfolio.start()
+      await depositController.setDepositAllowed(true, PortfolioStatus.Live)
+      await depositToTranche(tranche, loan.principal)
+
+      const addLoanTx = (await portfolio.addLoan(loan))
+      let totalCost = (await addLoanTx.wait()).gasUsed
+
+      const loanId: BigNumber = await extractEventArgFromTx(addLoanTx, [portfolio.address, 'LoanAdded', 'loanId'])
+      const acceptTx = await fixedInterestOnlyLoans.connect(other).acceptLoan(loanId)
+
+      const fundLoanTx = await portfolio.fundLoan(loanId)
+      totalCost = totalCost.add((await fundLoanTx.wait()).gasUsed)
+
+      console.log('Total gas used by manager: ', numberWithCommas(totalCost.toString()))
+      console.log('Gas used by borrower for loan accept: ', numberWithCommas((await acceptTx.wait()).gasUsed.toString()))
+    })
+
+    it('repay loan', async () => {
+      const {
+        structuredPortfolioFactory,
+        token,
+        fixedInterestOnlyLoans,
+        portfolioParams,
+        tranchesInitData,
+        expectedEquityRate,
+        wallet,
+        other,
+        loan,
+        getTranchesFromTx,
+        getPortfolioFromTx,
+        depositToTranche,
+      } = await loadFixture(structuredPortfolioLiveFixture)
+
+      const createPortfolioTx = await structuredPortfolioFactory.createPortfolio(
+        token.address,
+        fixedInterestOnlyLoans.address,
+        portfolioParams,
+        [tranchesInitData[0]],
+        expectedEquityRate,
+      )
+      const portfolio = await getPortfolioFromTx(createPortfolioTx)
+      const [tranche] = await getTranchesFromTx(createPortfolioTx)
+      const depositControllerAddress = await tranche.depositController()
+      const depositController = DepositController__factory.connect(depositControllerAddress, wallet)
+      await depositController.setCeiling(loan.principal)
+      await portfolio.start()
+      await depositController.setDepositAllowed(true, PortfolioStatus.Live)
+      await depositToTranche(tranche, loan.principal)
+
+      const addLoanTx = (await portfolio.addLoan(loan))
+      await addLoanTx.wait()
+      const loanId: BigNumber = await extractEventArgFromTx(addLoanTx, [portfolio.address, 'LoanAdded', 'loanId'])
+      await fixedInterestOnlyLoans.connect(other).acceptLoan(loanId)
+      await portfolio.fundLoan(loanId)
+
+      await token.connect(other).approve(portfolio.address, constants.MaxUint256)
+      const repayLoanTx = await portfolio.connect(other).repayLoan(loanId)
+
+      await printGasCost(await repayLoanTx)
+    })
+
+    it('change full depositController configuration on portfolio', async () => {
+      const {
+        structuredPortfolioFactory,
+        token,
+        fixedInterestOnlyLoans,
+        portfolioParams,
+        tranchesInitData,
+        expectedEquityRate,
+        wallet,
+        getTranchesFromTx,
+        getPortfolioFromTx,
+      } = await loadFixture(structuredPortfolioLiveFixture)
+
+      const createPortfolioTx = await structuredPortfolioFactory.createPortfolio(
+        token.address,
+        fixedInterestOnlyLoans.address,
+        portfolioParams,
+        [tranchesInitData[0]],
+        expectedEquityRate,
+      )
+      const portfolio = await getPortfolioFromTx(createPortfolioTx)
+      const [tranche] = await getTranchesFromTx(createPortfolioTx)
+      const depositControllerAddress = await tranche.depositController()
+      const depositController = DepositController__factory.connect(depositControllerAddress, wallet)
+      await depositController.setCeiling(400)
+      await portfolio.start()
+
+      const status = await portfolio.status()
+      const value = await depositController.depositAllowed(status)
+      const configureTx = await depositController.configure(
+        await (await depositController.ceiling()).add(100),
+        await (await depositController.depositFeeRate()).add(100),
+        await depositController.lenderVerifier(),
+        { status: status, value: !value },
+      )
+
+      await printGasCost(await configureTx)
+    })
+
+    it('change one field in depositController configuration on portfolio', async () => {
+      const {
+        structuredPortfolioFactory,
+        token,
+        fixedInterestOnlyLoans,
+        portfolioParams,
+        tranchesInitData,
+        expectedEquityRate,
+        wallet,
+        getTranchesFromTx,
+        getPortfolioFromTx,
+      } = await loadFixture(structuredPortfolioLiveFixture)
+
+      const createPortfolioTx = await structuredPortfolioFactory.createPortfolio(
+        token.address,
+        fixedInterestOnlyLoans.address,
+        portfolioParams,
+        [tranchesInitData[0]],
+        expectedEquityRate,
+      )
+      const portfolio = await getPortfolioFromTx(createPortfolioTx)
+      const [tranche] = await getTranchesFromTx(createPortfolioTx)
+      const depositControllerAddress = await tranche.depositController()
+      const depositController = DepositController__factory.connect(depositControllerAddress, wallet)
+      await depositController.setCeiling(400)
+      await portfolio.start()
+
+      const status = await portfolio.status()
+      const value = await depositController.depositAllowed(status)
+      const configureTx = await depositController.configure(
+        await (await depositController.ceiling()).add(100),
+        await depositController.depositFeeRate(),
+        await depositController.lenderVerifier(),
+        { status: status, value: value },
+      )
+
+      await printGasCost(await configureTx)
+    })
+
+    it('change full withdrawController configuration on portfolio', async () => {
+      const {
+        structuredPortfolioFactory,
+        token,
+        fixedInterestOnlyLoans,
+        portfolioParams,
+        tranchesInitData,
+        expectedEquityRate,
+        wallet,
+        getTranchesFromTx,
+        getPortfolioFromTx,
+      } = await loadFixture(structuredPortfolioLiveFixture)
+
+      const createPortfolioTx = await structuredPortfolioFactory.createPortfolio(
+        token.address,
+        fixedInterestOnlyLoans.address,
+        portfolioParams,
+        [tranchesInitData[0]],
+        expectedEquityRate,
+      )
+      const portfolio = await getPortfolioFromTx(createPortfolioTx)
+      const [tranche] = await getTranchesFromTx(createPortfolioTx)
+      const withdrawControllerAddress = await tranche.withdrawController()
+      const withdrawController = WithdrawController__factory.connect(withdrawControllerAddress, wallet)
+      await withdrawController.setFloor(400)
+      await portfolio.start()
+
+      const status = await portfolio.status()
+      const value = await withdrawController.withdrawAllowed(status)
+      const configureTx = await withdrawController.configure(
+        await (await withdrawController.floor()).add(100),
+        await (await withdrawController.withdrawFeeRate()).add(100),
+        { status: status, value: !value },
+      )
+
+      await printGasCost(await configureTx)
+    })
+
+    it('change one field in withdrawController configuration on portfolio', async () => {
+      const {
+        structuredPortfolioFactory,
+        token,
+        fixedInterestOnlyLoans,
+        portfolioParams,
+        tranchesInitData,
+        expectedEquityRate,
+        wallet,
+        getTranchesFromTx,
+        getPortfolioFromTx,
+      } = await loadFixture(structuredPortfolioLiveFixture)
+
+      const createPortfolioTx = await structuredPortfolioFactory.createPortfolio(
+        token.address,
+        fixedInterestOnlyLoans.address,
+        portfolioParams,
+        [tranchesInitData[0]],
+        expectedEquityRate,
+      )
+      const portfolio = await getPortfolioFromTx(createPortfolioTx)
+      const [tranche] = await getTranchesFromTx(createPortfolioTx)
+      const withdrawControllerAddress = await tranche.withdrawController()
+      const withdrawController = WithdrawController__factory.connect(withdrawControllerAddress, wallet)
+      await withdrawController.setFloor(400)
+      await portfolio.start()
+
+      const status = await portfolio.status()
+      const value = await withdrawController.withdrawAllowed(status)
+      const configureTx = await withdrawController.configure(
+        await (await withdrawController.floor()).add(100),
+        await withdrawController.withdrawFeeRate(),
+        { status: status, value: value },
+      )
+
+      await printGasCost(await configureTx)
+    })
   })
 })
