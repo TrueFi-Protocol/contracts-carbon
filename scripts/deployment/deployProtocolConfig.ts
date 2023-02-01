d4257682ac25a85b24792a57a07dbc501da3de1a
truefibot <truebot.github@trusttoken.com>
2023-02-01 13:35:42 +0000

    🪺 Add basic deployment script to Oxygen (#5482)

--- scripts/deployment/deployProtocolConfig.ts
+++ scripts/deployment/deployProtocolConfig.ts
@@ -6,7 +6,6 @@ import { config } from './config'
 
 export function deployProtocolConfig(networkName: string, prefix = '') {
   const implementation = contract(getNameWithPrefix(ProtocolConfig, prefix), ProtocolConfig)
-  // TODO update with proper addresses
   const { defaultProtocolFeeRate, pauserAddress, protocolAdmin, protocolTreasury } = config.protocolConfig[networkName]
   const initializeCallData = encodeInitializeCall(ProtocolConfig__factory, defaultProtocolFeeRate, protocolAdmin, protocolTreasury, pauserAddress)
   return proxy(implementation, initializeCallData)
