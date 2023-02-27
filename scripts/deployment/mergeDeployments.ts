9d4450074aad9e8af3961281b95ae4c9ff2eb44f
truefibot <truebot.github@trusttoken.com>
2023-02-27 08:08:51 +0000

    🧞‍♂️ Extract merge deployments to utils (#5733)

--- scripts/deployment/mergeDeployments.ts
+++ scripts/deployment/mergeDeployments.ts
@@ -1,13 +1,3 @@
-import { safeReadJsonFile, writeJsonFile } from 'deployments-utils'
-import { existsSync, mkdirSync, readdirSync } from 'fs'
-
-function mergeDeployments() {
-  if (!existsSync('./build')) {
-    mkdirSync('./build')
-  }
-  const deploymentsFiles = readdirSync('./').map(e => e.match(/^deployments-(.+)\.json$/)).filter(Boolean)
-  const merged = deploymentsFiles.reduce((prev, [file, network]) => ({...prev, [network]: safeReadJsonFile(file)[network] }), {})
-  writeJsonFile('build/deployments.json', merged)
-}
+import { mergeDeployments } from 'deployments-utils'
 
 mergeDeployments()
