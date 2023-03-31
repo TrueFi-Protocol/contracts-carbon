434b2d2f67ce80a8c9784d611a3f11b6b9d52366
truefibot <truebot.github@trusttoken.com>
2023-03-31 11:42:09 +0000

    👮‍♀️ Fail on failing fuzzing runs (#6113)

--- fuzzing/fuzz.py
+++ fuzzing/fuzz.py
@@ -3,17 +3,25 @@
 import os
 import argparse
 import json
+from subprocess import getstatusoutput
+import re
 
 def shell(cmd):
-    if os.system(cmd) != 0:
-      exit(1)
+    code, result = getstatusoutput(cmd)
+    print(result)
+    if code != 0:
+        exit(1)
+    return result
 
 def build_docker():
     shell(f"docker build -f fuzzing/docker/Dockerfile . -t fuzz_carbon")
 
-
 def spawn_single_container(conf):
-    shell(f"docker run --rm -e HOST_USER=$(id -u) -v {os.getcwd()}/echidna-corpus:/root/echidna-corpus fuzz_carbon {' '.join(conf)}")
+    output = shell(f"docker run --rm -e HOST_USER=$(id -u) -v {os.getcwd()}/echidna-corpus:/root/echidna-corpus fuzz_carbon {' '.join(conf)}")
+    if re.search("passed!", output) is None:
+        exit(1)
+    shell(f"rm -rf echidna-corpus/coverage")
+    shell(f"rm -rf echidna-corpus/reproducers")
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
