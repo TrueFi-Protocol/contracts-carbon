cf54dc484881ef1f6cde32915b18b51fe355abfd
truefibot <truebot.github@trusttoken.com>
2023-06-29 07:42:37 +0000

    🪝 Rewrite test to test something (#6250)

--- test/structuredPortfolio/deposit.test.ts
+++ test/structuredPortfolio/deposit.test.ts
@@ -7,45 +7,74 @@ import { timeTravelAndMine } from 'utils/timeTravel'
 describe('StructuredPortfolio.deposit', () => {
   const loadFixture = setupFixtureLoader()
 
-  it('simple token transfer does not make any effect', async () => {
-    const { structuredPortfolio, token, tranches, depositToTranche, parseTokenUnits, addAndFundLoan, getLoan, repayLoanInFull, tranchesData } = await loadFixture(structuredPortfolioFixture)
+  it('simple token transfer does not make any effect when updateCheckpoint is not called with existing deficit', async () => {
+    const waterfalls = {}
+
+    async function saveWaterfall(label = 'default') {
+      waterfalls[label] = await structuredPortfolio.calculateWaterfall()
+    }
+
+    async function compareWaterfall(label = 'default') {
+      const waterfall = await structuredPortfolio.calculateWaterfall()
+      waterfall.forEach((w, i) => expect(w).to.be.closeTo(waterfalls[label][i], 10))
+    }
+
+    let { structuredPortfolio, token, tranches, depositToTranche, parseTokenUnits, addAndFundLoan, getLoan, repayLoanInFull, tranchesData } = await loadFixture(structuredPortfolioFixture)
     await token.transfer(structuredPortfolio.address, 1234)
     expect(await structuredPortfolio.liquidAssets()).to.eq(0)
-
-    async function printWaterfall(label?: string) {
-      const waterfall = await structuredPortfolio.calculateWaterfall()
-      console.log(label || '')
-      console.log('Waterfall:')
-      console.log(waterfall.map((w) => w.toString()).join('\n'))
-      console.log('Deficits:')
-      console.log((await tranches[1].getCheckpoint()).deficit.toString())
-      console.log('\n')
-    }
 
     await depositToTranche(tranches[0], parseTokenUnits(100))
     await depositToTranche(tranches[1], parseTokenUnits(100))
     await depositToTranche(tranches[2], parseTokenUnits(100))
 
     await structuredPortfolio.start()
-    const loanId = await addAndFundLoan(getLoan({ periodDuration: 1, principal: parseTokenUnits(180) }))
+    let loanId = await addAndFundLoan(getLoan({ periodDuration: 1, principal: parseTokenUnits(180) }))
     await timeTravelAndMine(3 * DAY)
 
     await structuredPortfolio.markLoanAsDefaulted(loanId)
 
-    await printWaterfall()
+    await saveWaterfall()
 
     await timeTravelAndMine(YEAR)
 
-    await printWaterfall('before deposit')
+    await saveWaterfall('before deposit')
 
-    // await structuredPortfolio.updateCheckpoints()
     await tranchesData[1].depositController.setDepositAllowed(true, 1)
     await depositToTranche(tranches[1], parseTokenUnits(100))
 
-    await printWaterfall('after deposit')
+    await saveWaterfall('after deposit')
 
     await repayLoanInFull(loanId)
 
-    await printWaterfall()
+    await saveWaterfall('after repayment')
+
+    ;({ structuredPortfolio, token, tranches, depositToTranche, parseTokenUnits, addAndFundLoan, getLoan, repayLoanInFull, tranchesData } = await loadFixture(structuredPortfolioFixture))
+    await token.transfer(structuredPortfolio.address, 1234)
+
+    await depositToTranche(tranches[0], parseTokenUnits(100))
+    await depositToTranche(tranches[1], parseTokenUnits(100))
+    await depositToTranche(tranches[2], parseTokenUnits(100))
+
+    await structuredPortfolio.start()
+    loanId = await addAndFundLoan(getLoan({ periodDuration: 1, principal: parseTokenUnits(180) }))
+    await timeTravelAndMine(3 * DAY)
+
+    await structuredPortfolio.markLoanAsDefaulted(loanId)
+
+    await compareWaterfall()
+
+    await timeTravelAndMine(YEAR)
+
+    await compareWaterfall('before deposit')
+
+    await structuredPortfolio.updateCheckpoints() // The only difference between two flows
+    await tranchesData[1].depositController.setDepositAllowed(true, 1)
+    await depositToTranche(tranches[1], parseTokenUnits(100))
+
+    await compareWaterfall('after deposit')
+
+    await repayLoanInFull(loanId)
+
+    await compareWaterfall('after repayment')
   })
 })
